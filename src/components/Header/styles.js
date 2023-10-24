a18aea4b77fdeb8ca4debbb7ed1752ab761c88fd
Djhonatas Oliveira <djhonatas.oliveira@oswaldocruz.br>
2023-10-24 12:49:05 -0300

    corrigindo cabeçalho

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,13 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 150px;
-  text=aling: center;
-  background: teal
-  `
+  text-align: center;
+  background: teal;
+`;
 
-export const Header = styled.h1``
+export const Header = styled.h1`
+  /* Estilos para o cabeçalho, se necessário */
+`;
 
 export const Title = styled.div`
   padding-top: 20px;
   color: #fff;
-  `
+  /* Outros estilos para o título, se necessário */
+`;
